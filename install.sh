d749435d212e9fcf4fe73795ddbbc386ac4c1a66
Richard-Ulloa <71399502+Richard-Ulloa@users.noreply.github.com>
2021-02-08 15:50:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #bin/bash!
 sudo apt-get update
 sudo apt-get install squid
+sudo systemctl start squid
+sudo systemctl enable squid
 cp squid.conf /etc/squid/squid.conf
 cp extentions /etc/squid/
 cp blocksites /etc/squid/
